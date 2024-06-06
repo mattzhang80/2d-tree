977fecdb2dcdc9f98650febb71c5766c5275b17d
Matthew Zhang <115035711+mattzhang80@users.noreply.github.com>
2024-06-06 17:39:15 -0400

    Update PointST.java

--- PointST.java
+++ PointST.java
@@ -2,54 +2,54 @@ import edu.princeton.cs.algs4.Point2D;
 import edu.princeton.cs.algs4.RectHV;
 import edu.princeton.cs.algs4.Stack;
 import edu.princeton.cs.algs4.StdOut;
-
+ 
 import java.util.TreeMap;
-
+ 
 public class PointST<Value> {
     // creates Tree Map to represent a symbol table whose keys are 2D points
     private TreeMap<Point2D, Value> treeMap;
-
+ 
     // construct an empty symbol table of points
     public PointST() {
         treeMap = new TreeMap<>();
     }
-
+ 
     // is the symbol table empty?
     public boolean isEmpty() {
         return (treeMap.size() == 0);
     }
-
+ 
     // number of points
     public int size() {
         return treeMap.size();
     }
-
+ 
     // associate the value val with point p
     public void put(Point2D p, Value val) {
         if (p == null || val == null)
             throw new IllegalArgumentException("Point or value is null");
         treeMap.put(p, val);
     }
-
+ 
     // value associated with point p
     public Value get(Point2D p) {
         if (p == null)
             throw new IllegalArgumentException("Point is null");
         return treeMap.get(p);
     }
-
+ 
     // does the symbol table contain point p?
     public boolean contains(Point2D p) {
         if (p == null)
             throw new IllegalArgumentException("Point is null");
         return treeMap.containsKey(p);
     }
-
+ 
     // all points in the symbol table
     public Iterable<Point2D> points() {
         return treeMap.keySet();
     }
-
+ 
     // all points that are inside the rectangle (or on the boundary)
     public Iterable<Point2D> range(RectHV rect) {
         if (rect == null) throw new IllegalArgumentException("Rectangle is null");
@@ -61,7 +61,7 @@ public class PointST<Value> {
         }
         return ptStack;
     }
-
+ 
     // a nearest neighbor of point p; null if the symbol table is empty
     public Point2D nearest(Point2D p) {
         if (p == null)
@@ -80,36 +80,56 @@ public class PointST<Value> {
         }
         return nearest;
     }
-
+ 
     // unit testing (required)
     public static void main(String[] args) {
-        PointST<Point2D> testing = new PointST<Point2D>();
-        if (testing.isEmpty())
-            StdOut.println("isEmpty() working as intended");
-        else
-            StdOut.println("isEmpty() is bugged");
-        Point2D point1 = new Point2D(0.5, 0.5);
-        Point2D point2 = new Point2D(0.75, 0.75);
-        testing.put(point1, point1);
-        testing.put(point2, point2);
-        if (!testing.isEmpty())
-            StdOut.println("put() and isEmpty() working as intended");
-        else
-            StdOut.println("put() and/or isEmpty() is bugged");
-        if (testing.size() == 2)
-            StdOut.println("size() working as intended");
-        else
-            StdOut.println("size() is bugged");
-        if (testing.contains(point2))
-            StdOut.println("get() should output (0.75, 0.75): " + testing.get(point2));
-        else
-            StdOut.println("contains() is bugged");
-        StdOut.println("points() should output [(0.5, 0.5), (0.75, 0.75)]: "
-                               + testing.points());
-        RectHV rect1 = new RectHV(0.6, 0.6, 0.9, 0.9);
-        StdOut.println("rect() should output (0.75, 0.75): " + testing.range(rect1));
-        Point2D point3 = new Point2D(0.8, 0.8);
-        StdOut.println("nearest() should output (0.75, 0.75): "
-                               + testing.nearest(point3));
-    }
+        // creates a few 2D points
+        Point2D pt1 = new Point2D(0, 0);
+        Point2D pt2 = new Point2D(1, 0);
+        Point2D pt3 = new Point2D(0, 1);
+        Point2D pt4 = new Point2D(2, 2);
+ 
+        // creates two symbol tables
+        PointST<String> st1 = new PointST<String>();
+        PointST<Integer> st2 = new PointST<Integer>();
+ 
+        // creates two rectangles
+        RectHV rect1 = new RectHV(-1, -1, 4, 5);
+        RectHV rect2 = new RectHV(1, 1, 1, 1);
+ 
+        // puts points and keys into the ST
+        st1.put(pt1, "value1");
+        st1.put(pt2, "value2");
+        st1.put(pt3, "value3");
+        st1.put(pt4, "value4");
+ 
+        // tests methods by calling them, should match inquiry
+        StdOut.println("SHOULD RETURN FALSE: " + st1.isEmpty());
+        StdOut.println("SHOULD RETURN TRUE: " + st2.isEmpty());
+ 
+        StdOut.println("SHOULD RETURN 4: " + st1.size());
+        StdOut.println("SHOULD RETURN 0: " + st2.size());
+ 
+        StdOut.println("SHOULD RETURN value1: " + st1.get(pt1));
+        StdOut.println("SHOULD RETURN value4: " + st1.get(pt4));
+ 
+        Point2D pt10 = new Point2D(100, 100);
+ 
+        StdOut.println("SHOULD RETURN TRUE: " + st1.contains(pt1));
+        StdOut.println("SHOULD RETURN FALSE: " + st1.contains(pt10));
+ 
+        StdOut.println("SHOULD RETURN [(0.0, 0.0), (1.0, 0.0), (0.0, 1.0), "
+                               + "(2.0, 2.0)]: " + st1.points());
+ 
+        StdOut.println(
+                "SHOULD RETURN (2.0, 2.0) (0.0, 1.0) (1.0, 0.0) (0.0, 0.0): "
+                        + st1.range(rect1));
+ 
+        StdOut.println(
+                "SHOULD RETURN (2.0, 2.0) (0.0, 1.0) (1.0, 0.0) (0.0, 0.0) : "
+                        + st1.range(rect1));
+        StdOut.println("SHOULD RETURN NOTHING: " + st2.range(rect2));
+ 
+        StdOut.println("SHOULD RETURN (1.0, 0.0): " + st1.nearest(pt1));
+    } 
 }

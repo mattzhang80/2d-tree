11cbc0f22f3b752d69f46850bf897ec1f9dc0e96
Matthew Zhang <115035711+mattzhang80@users.noreply.github.com>
2024-06-06 17:38:33 -0400

    Update KdTreeST.java

--- KdTreeST.java
+++ KdTreeST.java
@@ -3,12 +3,12 @@ import edu.princeton.cs.algs4.Queue;
 import edu.princeton.cs.algs4.RectHV;
 import edu.princeton.cs.algs4.Stack;
 import edu.princeton.cs.algs4.StdOut;
-
+ 
 public class KdTreeST<Value> {
     private Node root; // Creates variable for root node
     private int pointCounter; // Tracks size/number of points
     private double inf = Double.POSITIVE_INFINITY; // infinity
-
+ 
     // Node representation adapted from K-d trees assignment
     // checklist (course material)
     private class Node {
@@ -17,7 +17,7 @@ public class KdTreeST<Value> {
         private Node rt;        // the right/top subtree
         private Value val;      // value
         private RectHV rect;    // rectangle
-
+ 
         // constructor for a node object
         public Node(Point2D p, Node lb, Node rt, Value val, RectHV rect) {
             this.p = p;
@@ -27,23 +27,23 @@ public class KdTreeST<Value> {
             this.val = val;
         }
     }
-
+ 
     // construct an empty symbol table of points
     public KdTreeST() {
         pointCounter = 0;
         root = null;
     }
-
+ 
     // is the symbol table empty?
     public boolean isEmpty() {
         return size() == 0;
     }
-
+ 
     // number of points
     public int size() {
         return pointCounter;
     }
-
+ 
     // associate the value val with point p, exception with null point
     public void put(Point2D p, Value val) {
         RectHV rect = new RectHV(-inf, -inf, inf, inf);
@@ -51,8 +51,8 @@ public class KdTreeST<Value> {
             throw new IllegalArgumentException("Calls put() with a null point");
         root = put(root, p, true, val, rect);
     }
-
-    // private helper method for put
+ 
+    // private helper method for put 
     private Node put(Node node, Point2D p, boolean vertical, Value val, RectHV rect) {
         // conditional if node is null, corner case
         if (node == null) {
@@ -65,12 +65,12 @@ public class KdTreeST<Value> {
         double nodey = node.p.y();
         double px = p.x();
         double py = p.y();
-
+ 
         if (nodex == px && nodey == py) {
             node.val = val;
             return node;
         }
-
+ 
         // conditional put if node is vertical
         if (vertical) {
             if (px >= nodex) {
@@ -95,14 +95,14 @@ public class KdTreeST<Value> {
         }
         return node;
     }
-
+ 
     // value associated with point p, exception w/null point
     public Value get(Point2D p) {
         if (p == null)
             throw new IllegalArgumentException("Point is null");
         return get(root, p, true);
     }
-
+ 
     // private helper method for get
     private Value get(Node node, Point2D p, boolean vert) {
         // conditional get if node is null or point equal to node point
@@ -125,12 +125,12 @@ public class KdTreeST<Value> {
                 return get(node.rt, p, true);
         }
     }
-
+ 
     // does the symbol table contain point p?
     public boolean contains(Point2D p) {
         return get(p) != null;
     }
-
+ 
     // all points in the symbol table
     public Iterable<Point2D> points() {
         // initialize queues to track nodes and points
@@ -148,7 +148,7 @@ public class KdTreeST<Value> {
         }
         return keyQueue;
     }
-
+ 
     // all points that are inside the rectangle (or on the boundary)
     public Iterable<Point2D> range(RectHV rect) {
         if (rect == null) throw new IllegalArgumentException("Rectangle is null");
@@ -157,7 +157,7 @@ public class KdTreeST<Value> {
         insert(root, rect, rangeStack);
         return rangeStack;
     }
-
+ 
     // private helper method to add to range of points inside
     private void insert(Node node, RectHV rect, Stack<Point2D> rangeStack) {
         // conditional that adds points to range stack if boundary or in rectangle
@@ -169,7 +169,7 @@ public class KdTreeST<Value> {
             insert(node.rt, rect, rangeStack);
         }
     }
-
+ 
     // a nearest neighbor of point p; null if the symbol table is empty
     public Point2D nearest(Point2D p) {
         if (p == null) throw new IllegalArgumentException("Rectangle is null");
@@ -179,7 +179,7 @@ public class KdTreeST<Value> {
             return nearest(root, p, null, true);
         }
     }
-
+ 
     // returns nearest neighbor to query point
     private Point2D nearest(Node node, Point2D p, Point2D near, boolean vert) {
         // when code reaches null node, this is pruning
@@ -194,17 +194,17 @@ public class KdTreeST<Value> {
             if (node.p.distanceSquaredTo(p) < near.distanceSquaredTo(p))
                 near = node.p;
         }
-
+ 
         double compare = compare(p, node.p, vert);
         // recursion to test left/top and right/bottom subtrees
         if (compare < 0) {
-            // added if statements before each recursion per lab TA reccomendation
+            // added if statements before each recursion per lab TA reccomendation 
             if (near.distanceSquaredTo(p) < node.rect.distanceSquaredTo(p)) {
                 return near;
             }
             if (node.p.distanceSquaredTo(p) < near.distanceSquaredTo(p))
                 near = node.p;
-
+ 
             near = nearest(node.lb, p, near, !vert);
             if (near.distanceSquaredTo(p) < node.rect.distanceSquaredTo(p)) {
                 return near;
@@ -229,7 +229,7 @@ public class KdTreeST<Value> {
         }
         return near;
     }
-
+ 
     // private helper method to compare x or y value of query and node point
     private double compare(Point2D p, Point2D nodeP, boolean vert) {
         if (vert) {
@@ -239,7 +239,7 @@ public class KdTreeST<Value> {
             return p.y() - nodeP.y();
         }
     }
-
+ 
     // unit testing (required)
     public static void main(String[] args) {
         KdTreeST<String> testing = new KdTreeST<>();
@@ -266,16 +266,16 @@ public class KdTreeST<Value> {
         StdOut.println("All points: " + testing.points());
         RectHV rect1 = new RectHV(0.6, 0.6, 0.9, 0.9);
         StdOut.println("test rectangle contains " + testing.range(rect1));
-
+ 
         Point2D point3 = new Point2D(0.8, 0.8);
         testing.put(point3, "value3");
         StdOut.println("nearest point to (0.8, 0.8) is " + testing.nearest(point3));
-
+ 
         StdOut.println("--------- after adding another point 3 ---------");
         StdOut.println("Points after one put: " + testing.points());
         StdOut.println("Size after one put: " + testing.size());
         StdOut.println("Emptiness after one put: " + testing.isEmpty());
-
+ 
         StdOut.println("--------- now we insert general pts ---------");
         KdTreeST<String> testing2 = new KdTreeST<>();
         Point2D genPoint1 = new Point2D(0.5, 0.5);
@@ -286,7 +286,7 @@ public class KdTreeST<Value> {
         StdOut.println("Size after general put: " + testing2.size());
         StdOut.println("Emptiness after general put: " + testing2.isEmpty());
         StdOut.println("Value after general put: " + testing2.root.val);
-
+ 
         StdOut.println("--------- now we test sequence of nodes called in tree -----");
         Point2D p1 = new Point2D(0.7, 0.2);
         Point2D p2 = new Point2D(0.5, 0.4);
@@ -301,7 +301,7 @@ public class KdTreeST<Value> {
         testing3.put(p5, "E");
         Point2D query = new Point2D(0.69, 0.96);
         StdOut.println("Nearest point:" + testing3.nearest(query));
-
+ 
         Point2D pp1 = new Point2D(0.372, 0.497);
         Point2D pp2 = new Point2D(0.564, 0.413);
         Point2D pp3 = new Point2D(0.226, 0.577);
@@ -312,7 +312,7 @@ public class KdTreeST<Value> {
         Point2D pp8 = new Point2D(0.862, 0.825);
         Point2D pp9 = new Point2D(0.785, 0.725);
         Point2D pp10 = new Point2D(0.499, 0.208);
-
+ 
         StdOut.println("------another sequence test-----");
         KdTreeST<String> test4 = new KdTreeST<>();
         test4.put(pp1, "A");
@@ -327,5 +327,5 @@ public class KdTreeST<Value> {
         test4.put(pp10, "J");
         Point2D qu = new Point2D(0.6, 0.53);
         StdOut.println("Nearest point:" + test4.nearest(qu));
-    }
+    } 
 }
